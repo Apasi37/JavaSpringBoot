cebd83e2bd21f9bb4bf1dc3982c137150a2af720
Apasi37 <161586519+Apasi37@users.noreply.github.com>
2025-10-17 08:36:02 +0200

    Delete uloha3 directory

--- uloha3/src/main/java/sk/ukf/uloha3/dao/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package sk.ukf.uloha3.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sk.ukf.uloha3.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-    //nie je potrebné písať žiaden kód
-}
